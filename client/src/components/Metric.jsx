0a0992dc9644760d807d0f8d3197f863072a6ce2
Mohid-Anwar <mohid.anwar@gmail.com>
2024-09-13 21:49:15 +0500

    Metric Carousel Updated

--- client/src/components/Metric.jsx
+++ client/src/components/Metric.jsx
@@ -13,24 +13,29 @@ const Metric = () => {
     fetchCars();
   }, []);
 
+  
+
   return (
-    <div className="flex items-center justify-center pt-6 pb-2">
-      <div className="carousel rounded-box w-64 ">
+    <div className="flex items-center justify-center pt-6 pb-6">
+      <div className="carousel w-64 ">
         {cars &&
           cars.map((car) => {
             return (
-              <div
-                className="carousel-item w-full border border-white"
-                key={car._id}
-              >
-                <h2 className="text-black text-center text-xl">
-                  {car.general.brand}
-                </h2>
-                <img
-                  src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-                  className="w-full"
-                  alt="Tailwind CSS Carousel component"
-                />
+              <div className="carousel-item w-full" key={car._id}>
+                <div className="relative group cursor-pointer overflow-hidden duration-500 w-64 h-80 bg-zinc-800 text-gray-50 p-5">
+                  <div className="">
+                    <div className="group-hover:scale-110 w-full h-60 bg-blue-400 duration-500" />
+                    <div className="absolute w-56 left-0 p-5 -bottom-16 duration-500 group-hover:-translate-y-12">
+                      <div className="absolute -z-10 left-0 w-64 h-28 opacity-0 duration-500 group-hover:opacity-50 group-hover:bg-blue-900" />
+                      <span className="text-xl font-bold">
+                        {car.general.brand}
+                      </span>
+                      <p className="group-hover:opacity-100 w-56 duration-500 opacity-0">
+                        {car.general.model}
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
             );
           })}
